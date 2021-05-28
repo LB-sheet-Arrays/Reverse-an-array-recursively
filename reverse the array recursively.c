046e5b206052bbce6bd5e7fc0ffb669fcc06b46e
Atharva Deshmukh <81254891+Atharva-Deshmukh@users.noreply.github.com>
2021-05-28 22:08:23 +0530

    Add files via upload

---
+++ reverse the array recursively.c
@@ -0,0 +1,43 @@
+//use two pointers and recursively increment them
+
+#include<stdio.h>
+
+void reverse_array(int a[],int left,int right)
+{
+	int temp=0;
+	if(left<right) //re recusrion khatam hone ki condition hai ((corner condition))
+	{
+		temp=a[left];
+		a[left]=a[right]; //swapping part
+		a[right]=temp;
+		
+		reverse_array(a,left+1,right-1);
+	}
+}
+
+int main()
+{
+	int a[100],i,n;
+	
+	printf("\nHow many elements do you want in your array?");
+	scanf("%d",&n);
+	
+	printf("\nFeed the array plz ");
+	for(i=0;i<n;i++)
+	{
+		printf("\nElement no %d => ",i+1);
+		scanf("%d",&a[i]);
+	}
+	
+	//call our function
+	reverse_array(a,0,n-1);
+	
+	printf("\n\nThe recursively sorted array is \n { ");
+	for(i=0;i<n;i++)
+	{
+		printf(" %d ",a[i]);
+	}
+	printf(" } ");
+	
+	return 0;
+}
